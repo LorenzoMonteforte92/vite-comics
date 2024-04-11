9a31267869f71ee735dc671b6a613465ef64a2d0
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-11 14:42:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  
+
+ export default {
+  components: { 
+    AppHeader,
+  
+  }
+ }
+
+</script>
+
+<template>
+  <div class="container">
+    <AppHeader></AppHeader>
+  </div>
+</template>
+
+<style lang="scss">
+  @use './style/generic'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,75 @@
+<script>
+    export default {
+        name: 'AppHeader',
+
+        data(){
+            return{
+                links:[
+                    {
+                        text: `CHARACTERS`,
+                        active: false
+                    },
+                    {
+                        text: `COMICS`,
+                        active: false
+                    },
+                    {
+                        text: `MOVIES`,
+                        active: false
+                    },
+                    {
+                        text: `TV`,
+                        active: false
+                    },
+                    {
+                        text: `GAMES`,
+                        active: false
+                    },
+                    {
+                        text: `COLLECTIBLES`,
+                        active: false
+                    },
+                    {
+                        text: `VIDEOS`,
+                        active: false
+                    },
+                    {
+                        text: `FANS`,
+                        active: false
+                    },
+                    {
+                        text: `NEWS`,
+                        active: false
+                    },
+                    {
+                        text: `SHOP`,
+                        active: false
+                    },
+                ]
+            }
+        },
+        methods: {
+            
+            }
+        }
+
+    
+</script>
+
+<template>
+    <div class="row justify-content-between">
+        <div class="col-2"></div>
+        <div class="col-7">
+        <nav>
+            <ul>
+                <li><a href="#">ciao</a></li>
+                <li><a href="#">ciao</a></li>
+            </ul>
+        </nav>
+    </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
