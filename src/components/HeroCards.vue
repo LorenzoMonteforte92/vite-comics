d02326e54c0f082486c6bebedbf834e3d5474614
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-12 13:24:49 +0200

    feat: add hero image

--- src/components/HeroCards.vue
+++ src/components/HeroCards.vue
@@ -17,7 +17,7 @@
 </script>
 
 <template>
-
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, eveniet quos possimus saepe minima optio obcaecati repellat earum aperiam reiciendis harum quidem delectus? Quo laboriosam, maxime dolorem pariatur expedita quisquam.
 </template>
 
 <style scoped lang="scss">
